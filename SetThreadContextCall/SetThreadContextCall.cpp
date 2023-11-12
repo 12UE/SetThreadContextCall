b110f44e23fd868494821844175733495e54cef0
12UE <1258432472@qq.com>
2023-11-12 16:55:14 +0800

    更新

--- SetThreadContextCall/SetThreadContextCall.cpp
+++ SetThreadContextCall/SetThreadContextCall.cpp
@@ -577,13 +577,13 @@ private:
         auto hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
         if (hSnapshot != INVALID_HANDLE_VALUE) {
             PROCESSENTRY32W processEntry = { sizeof(PROCESSENTRY32W), };
-            if (Process32First(hSnapshot, &processEntry)) {
+            if (Process32FirstW(hSnapshot, &processEntry)) {
                 do {
                     if (_ucsicmp(processEntry.szExeFile, processName) == 0) {
                         pid = processEntry.th32ProcessID;
                         break;
                     }
-                } while (Process32Next(hSnapshot, &processEntry));
+                } while (Process32NextW(hSnapshot, &processEntry));
             }
             CloseHandle(hSnapshot);
         }
