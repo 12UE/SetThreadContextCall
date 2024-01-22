b12e3981543cbc969f8a8c81f88c762e8e73837d
12UE <1258432472@qq.com>
2024-01-22 12:58:01 +0800

    更新

--- SetThreadContextCall/SetThreadContextCall.cpp
+++ SetThreadContextCall/SetThreadContextCall.cpp
@@ -1151,8 +1151,7 @@ private:
 int main(){
     auto& Process = Process::GetInstance();//get instance   获取实例
     Process.Attach("notepad.exe");//attach process  附加进程
-    while (true)
-    {
+    while (true){
         std::cout << Process.SetContextCall(MessageBoxA, Process::TONULL<HWND>(), "MSG", "CAP", MB_OK).get();
     }
     return 0;
