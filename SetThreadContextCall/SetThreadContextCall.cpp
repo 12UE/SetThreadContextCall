371047c7ee93b0b365c8f7c48ef49775de080f30
12UE <1258432472@qq.com>
2024-02-26 09:16:27 +0800

    更新

--- SetThreadContextCall/SetThreadContextCall.cpp
+++ SetThreadContextCall/SetThreadContextCall.cpp
@@ -2,50 +2,34 @@
 using namespace stc;
 //定义一个void类型的函数指针
 typedef void(*pFunc)();
-void startProcessIfNotFound(const wchar_t* exeName) {
+void startProcessIfNotFound(const char* exeName) {
     // 创建进程快照
-    auto findprocess = [&](const wchar_t* processName)->bool{
-        PROCESSENTRY32 pe32{};
+    auto findprocess = [&](const char* processName)->bool{
+        PROCESSENTRY32W pe32{ sizeof(PROCESSENTRY32W) ,};
         GenericHandle<HANDLE, NormalHandle> hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-        if (hProcessSnap == INVALID_HANDLE_VALUE) {
-            std::cerr << "Failed to create process snapshot." << std::endl;
-            return false;
-        }
-
-        // 设置结构体大小
-        pe32.dwSize = sizeof(PROCESSENTRY32);
-
-        // 获取第一个进程信息
-        if (!Process32First(hProcessSnap, &pe32)) {
-            std::cerr << "Failed to get process info." << std::endl;
-            return false;
-        }
-
+        if (!hProcessSnap)return false;
         bool found = false;
-        // 遍历进程快照，查找指定的exeName
-        do {
-            if (wcscmp(pe32.szExeFile, exeName) == 0) {
+        for (BOOL bRet = Process32FirstW(hProcessSnap, &pe32); bRet; bRet = Process32NextW(hProcessSnap, &pe32)) {
+            if (_ucsicmp(pe32.szExeFile, exeName)) {
                 found = true;
                 break;
             }
-        } while (Process32Next(hProcessSnap, &pe32));
+        }
         return found;
-     };
-    
+    };
     auto found = findprocess(exeName);
     if (!found) {
-        ShellExecute(NULL, L"open", exeName, NULL, NULL, SW_SHOWNORMAL);
+        ShellExecuteA(NULL, "open", exeName, NULL, NULL, SW_SHOWNORMAL);
         Sleep(1000);
         std::cout << "sleeped done!" << "\n";
-    }
-    else {
-        std::wcout << "Process " << exeName << " is already running." << std::endl;
+    }else {
+        std::cout << "Process " << exeName << " is already running." << std::endl;
     }
 }
 int main() {
     auto& Process = Process::GetInstance();//get instance
     //判断当前有没有运行记事本
-    startProcessIfNotFound(L"notepad.exe");
+    startProcessIfNotFound("notepad.exe");
 
     Process.Attach("notepad.exe");//attach process
     Process.SetContextCall(MessageBoxA, Process::TONULL<HWND>(), "cap", "ok", MB_OK);
